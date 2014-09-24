7e706923752561061fe434adc4bc98519e7ed298
MrHuxu <hxtheone@gmail.com>
2014-09-24 10:49:27 +0800

    edit the sublime plugin

--- plugins/sublime/sublime.plugin.zsh
+++ plugins/sublime/sublime.plugin.zsh
@@ -21,10 +21,10 @@ elif  [[ $('uname') == 'Darwin' ]]; then
     local _sublime_darwin_paths > /dev/null 2>&1
     _sublime_darwin_paths=(
         "/usr/local/bin/subl"
-        "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+#        "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
         "/Applications/Sublime Text 3.app/Contents/SharedSupport/bin/subl"
         "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
-        "$HOME/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+#        "$HOME/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
         "$HOME/Applications/Sublime Text 3.app/Contents/SharedSupport/bin/subl"
         "$HOME/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
     )
