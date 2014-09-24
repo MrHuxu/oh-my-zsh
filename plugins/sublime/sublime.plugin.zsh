8a6c864ab8a5ac3da72469551e4362762251c89d
MrHuxu <hxtheone@gmail.com>
2014-09-24 10:51:21 +0800

    edited the sublime plugin

--- plugins/sublime/sublime.plugin.zsh
+++ plugins/sublime/sublime.plugin.zsh
@@ -21,12 +21,12 @@ elif  [[ $('uname') == 'Darwin' ]]; then
     local _sublime_darwin_paths > /dev/null 2>&1
     _sublime_darwin_paths=(
         "/usr/local/bin/subl"
-#        "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
-        "/Applications/Sublime Text 3.app/Contents/SharedSupport/bin/subl"
         "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
-#        "$HOME/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
-        "$HOME/Applications/Sublime Text 3.app/Contents/SharedSupport/bin/subl"
+        "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+        "/Applications/Sublime Text 3.app/Contents/SharedSupport/bin/subl"
         "$HOME/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
+        "$HOME/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+        "$HOME/Applications/Sublime Text 3.app/Contents/SharedSupport/bin/subl"
     )
 
     for _sublime_path in $_sublime_darwin_paths; do
